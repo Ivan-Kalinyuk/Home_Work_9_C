cee391fe1b8d8a7719c16e68cea66dba40ce8d54
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-09-11 00:34:45 +0300

    Решение №66

---
+++ №66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите  значение M: ");
+int M = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите  значение N (большее, чем M): ");
+int N = Convert.ToInt32 (Console.ReadLine());
+int sum = 0;
+int p = N-M+1;
+for (int i = 0; i < p; i++)
+{
+    sum = sum + M;
+    M++;
+}
+Console.WriteLine($"Сумма чисел от M до N = {sum}");
