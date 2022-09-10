9c3a5fe98d208ddec3a6a2beab7e36c21dd1d124
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-09-11 00:23:34 +0300

    Решение №64

---
+++ №64/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите значение M: ");
+int M = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите значение N (должно быть больше M): ");
+int N = Convert.ToInt32 (Console.ReadLine());
+int [] ar = new int [N-M+1];
+for (int i = 0; i<ar.Length; i++ )
+{
+    ar [i] = ar [i] + M;
+    M++;
+    Console.Write ($"{ar[i]} , ");
+}
+
